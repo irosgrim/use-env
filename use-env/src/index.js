7095c30c01b88cdd4ce0aa33050e1a663b96b1d9
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2024-04-29 23:13:37 +0200

    custom action

--- use-env/src/index.js
+++ use-env/src/index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
+const yaml = require('js-yaml');
 
 try {
   const branchName = core.getInput('branch');
-  const envsInput = core.getInput('envs');
-  const environments = JSON.parse(envsInput);
+  const varsInput = core.getInput('variables');
+  const environments = yaml.load(varsInput);
 
   const environment = environments.find(env => env.branch === branchName);
   if (!environment) {
@@ -17,9 +18,35 @@ try {
     }
   });
 
-  console.log(`Setting variables for BRANCH: ${branchName}`);
+  console.log(`Setting variables for branch: ${branchName}`);
   console.log(`DONE`);
-
 } catch (error) {
-  core.setFailed(`Failed to process input or perform action: ${error.message}`);
+  core.setFailed(`Error processing YAML input: ${error.message}`);
 }
+
+
+// const core = require('@actions/core');
+
+// try {
+//   const branchName = core.getInput('branch');
+//   const envsInput = core.getInput('envs');
+//   const environments = JSON.parse(envsInput);
+
+//   const environment = environments.find(env => env.branch === branchName);
+//   if (!environment) {
+//     core.setFailed(`No variables provided for branch: ${branchName}`);
+//     return;
+//   }
+
+//   Object.keys(environment).forEach(key => {
+//     if (key !== 'branch') {
+//       core.setOutput(key, environment[key]);
+//     }
+//   });
+
+//   console.log(`Setting variables for branch: ${branchName}`);
+//   console.log(`DONE`);
+
+// } catch (error) {
+//   core.setFailed(`Failed to process input or perform action: ${error.message}`);
+// }
