14b1971de60d208412e3cf5321b1bcbc66ce23bf
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2024-04-30 00:33:38 +0200

    init

--- use-env/src/index.js
+++ use-env/src/index.js
@@ -1,17 +1,12 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const yaml = require('js-yaml');
 
 try {
-  const ref = github.context.ref;
-  const branchName = ref.replace('refs/heads/', '');
-  console.log(branchName);
-
+  const branchName = "main" //core.getInput('branch');
   const varsInput = core.getInput('variables');
   const environments = yaml.load(varsInput);
 
-  console.log({varsInput})
-  console.log(environments);
+  console.log(varsInput);
 
   const environment = environments.find(env => env.branch === branchName);
   if (!environment) {
@@ -30,3 +25,30 @@ try {
 } catch (error) {
   core.setFailed(`Error processing YAML input: ${error.message}`);
 }
+
+
+// const core = require('@actions/core');
+
+// try {
+//   const branchName = core.getInput('branch');
+//   const envsInput = core.getInput('envs');
+//   const environments = JSON.parse(envsInput);
+
+//   const environment = environments.find(env => env.branch === branchName);
+//   if (!environment) {
+//     core.setFailed(`No variables provided for branch: ${branchName}`);
+//     return;
+//   }
+
+//   Object.keys(environment).forEach(key => {
+//     if (key !== 'branch') {
+//       core.setOutput(key, environment[key]);
+//     }
+//   });
+
+//   console.log(`Setting variables for branch: ${branchName}`);
+//   console.log(`DONE`);
+
+// } catch (error) {
+//   core.setFailed(`Failed to process input or perform action: ${error.message}`);
+// }
