5fb941e44eaf56b63240e4315204a04abfb7c7dc
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2024-04-29 22:04:47 +0200

    nm

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const {getMultilineInput, setFailed, setOutput} = require("@actions/core");
+
+async function run() {
+    const envs = getMultilineInput("envs");
+    try {
+        if (!envs) {
+            throw new Error("No envs specified");
+        }
+        console.log({envs});
+        setOutput("some-env", "value-xxxx");
+        
+    } catch (error) {
+        setFailed(`Couldn't do much: ${error}`);
+    }
+}
+
+run();
